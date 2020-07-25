8959349bdae571d551681146337e4172e89ec589
1020536 <68794064+1020536@users.noreply.github.com>
2020-07-26 01:10:04 +0200

    Add files via upload

--- DeLegibusbigrams.R
+++ DeLegibusbigrams.R
@@ -2,8 +2,8 @@ library(data.table)
 library(tidyverse)
 library(tidytext)
 
-my_data <- read.delim('delegibus.txt', col.names=c("value"))
-bigrams <- my_data %>%
+delegibus <- read.delim('delegibus.txt', col.names=c("value"))
+bigrams <- delegibus %>%
   select(value) %>%
   unnest_tokens(ngram, value, token = "ngrams", n = 2) %>%
   count(ngram, sort = TRUE) %>%
